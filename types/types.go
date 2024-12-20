058452f5689eef61a5a7dba23b16924008156646
Megidy <byrka.sergiy@gmail.com>
2024-12-20 18:17:00 +0200

    Added dependencies and modified struct.

--- types/types.go
+++ types/types.go
@@ -1,7 +1,17 @@
 package types
 
+import "github.com/gin-gonic/gin"
+
+type Topic struct {
+	TopicID string
+	UserID  string
+	Name    string
+}
 type Question struct {
 	Id            string
+	Topic         *Topic
+	Type          string
+	ImageLink     string
 	Question      string
 	Answers       []string
 	CorrectAnswer string
@@ -9,4 +19,13 @@ type Question struct {
 type Message struct {
 	Headers map[string]any
 	Answer  map[string]string
+}
+
+type ClientSideHandler interface {
+	GetDataFromForm(context *gin.Context, key string) string
+}
+
+type GameStore interface {
+	CreateTopic(questions []Question) error
+	GetUsersTopics(userID string) ([]Topic, error)
 }
