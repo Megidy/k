6af7aeb0446b793721910a9e7caaf6c29c2e45b2
Megidy <byrka.sergiy@gmail.com>
2024-12-21 00:02:17 +0200

    Added new method for getting question by topic.

--- pkg/services/game/store.go
+++ pkg/services/game/store.go
@@ -60,3 +60,51 @@ func (s *store) GetUsersTopics(userID string) ([]types.Topic, error) {
 
 	return topics, nil
 }
+
+func (s *store) GetQuestionsByTopicName(TopicName string) ([]types.Question, error) {
+	var questions []types.Question
+	row, err := s.db.Query("select * from topics where name=?", TopicName)
+	if err != nil {
+		log.Println("error with topics")
+		return nil, err
+	}
+	var t types.Topic
+	for row.Next() {
+
+		err = row.Scan(&t.TopicID, &t.UserID, &t.Name)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	rows, err := s.db.Query("select id,type,image_link,question,correct_answer from questions where topic_id=?", t.TopicID)
+	if err != nil {
+		log.Println("error with questions")
+		return nil, err
+	}
+	for rows.Next() {
+		var q types.Question
+		q.Topic = &t
+		// q.Answers = make([]string, 4)
+		err = rows.Scan(&q.Id, &q.Type, &q.ImageLink, &q.Question, &q.CorrectAnswer)
+		if err != nil {
+			return nil, err
+		}
+		rows, err := s.db.Query("select answer from answers where question_id=?", q.Id)
+		if err != nil {
+			log.Println("error with answers ")
+			return nil, err
+		}
+
+		for rows.Next() {
+			var a string
+			err = rows.Scan(&a)
+			if err != nil {
+				return nil, err
+			}
+			q.Answers = append(q.Answers, a)
+		}
+		questions = append(questions, q)
+	}
+	return questions, nil
+}
