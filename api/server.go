0feaaaeb46bdfd4500533d1d9a69a1ecd8c85348
Megidy <byrka.sergiy@gmail.com>
2024-12-20 18:15:54 +0200

    Added new injections

--- api/server.go
+++ api/server.go
@@ -1,24 +1,31 @@
 package api
 
 import (
+	"database/sql"
+
 	"github.com/Megidy/k/pkg/services/game"
+	"github.com/Megidy/k/static/client"
 	"github.com/gin-gonic/gin"
 )
 
 type Server struct {
 	addr string
+	db   *sql.DB
 }
 
-func NewServer(addr string) *Server {
+func NewServer(addr string, db *sql.DB) *Server {
 	return &Server{
 		addr: addr,
+		db:   db,
 	}
 }
 
 func (s *Server) Run() error {
 	router := gin.Default()
-	manager := game.NewManager()
-	handler := game.NewGameHandler(manager)
+	clientSideHandler := client.NewClientSideHandler()
+	gameStore := game.NewGameStore(s.db)
+	handler := game.NewGameHandler(clientSideHandler, gameStore)
+
 	handler.RegisterRoutes(router)
 
 	return router.Run(s.addr)
