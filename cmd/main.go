341c0936b041e365b375159d3086097a19373a44
Megidy <byrka.sergiy@gmail.com>
2024-12-20 18:16:01 +0200

    Added db connection

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,17 @@ import (
 	"log"
 
 	"github.com/Megidy/k/api"
+	"github.com/Megidy/k/db"
 )
 
 func main() {
-	server := api.NewServer(":8080")
-	err := server.Run()
+	db, err := db.NewDB()
+	if err != nil {
+		log.Fatalln("error when establishing connection to db : ", err)
+	}
+	log.Println("started DB successfully")
+	server := api.NewServer(":8080", db)
+	err = server.Run()
 	if err != nil {
 		log.Fatalln("error while hosting server :", err)
 	}
