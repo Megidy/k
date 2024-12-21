35aca1473b7f3c71ef8dd0b7acde7e96be516aa5
Megidy <byrka.sergiy@gmail.com>
2024-12-21 19:47:23 +0200

    Added new method for gettin user by email.

--- pkg/services/user/store.go
+++ pkg/services/user/store.go
@@ -28,3 +28,35 @@ func (s *store) GetUserById(id string) (*types.User, error) {
 	}
 	return &user, nil
 }
+func (s *store) CreateUser(user *types.User) error {
+	_, err := s.db.Exec("insert into users values(?,?,?,?)", user.ID, user.UserName, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (s *store) UserExists(user *types.User) (bool, error) {
+	rows, err := s.db.Query("select * from users where email=? or username=?", user.Email, user.UserName)
+	if err != nil {
+		return false, err
+	}
+	for !rows.Next() {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (s *store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.db.Query("select * from users where email=?", email)
+	if err != nil {
+		return nil, err
+	}
+	var user types.User
+	for rows.Next() {
+		err = rows.Scan(&user.ID, &user.UserName, &user.Email, &user.Password)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &user, nil
+}
