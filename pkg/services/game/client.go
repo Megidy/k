d773b75e5c0953e71f91a9fef3cf28a7e186e308
Megidy <byrka.sergiy@gmail.com>
2024-12-22 22:45:16 +0200

    Added 2 new fields to struct : 1: writeWaitCh to send list of client that didnt answer the question yet 2: lenOfWait to break from the loop not to overwrite all the time previous clients.
    Deleted some logs and create new ones
    
    Added implemetation of writeWaitCh

--- pkg/services/game/client.go
+++ pkg/services/game/client.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"encoding/json"
 	"log"
-	"time"
 
 	"github.com/Megidy/k/static/components"
 	"github.com/Megidy/k/types"
@@ -13,44 +12,44 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-const (
-	// Time allowed to write a message to the peer.
-	writeWait = 10 * time.Second
+// const (
+// // Time allowed to write a message to the peer.
+//  writeWait = 10 * time.Second
 
-	// Time allowed to read the next pong message from the peer.
-	pongWait = 5 * time.Second
+// // Time allowed to read the next pong message from the peer.
+//  pongWait = 5 * time.Second
 
-	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = 1 * time.Second
+// //Send pings to peer with this period. Must be less than pongWait.
+// pingPeriod = 1 * time.Second
 
-	// Maximum message size allowed from peer.
-	maxMessageSize = 512
-)
+// // Maximum message size allowed from peer.
+//  maxMessageSize = 512
+// )
 
 type Client struct {
-	ginCtx   *gin.Context
-	userName string
-	conn     *websocket.Conn
-	manager  *Manager
-	question types.Question
-	exitCh   chan struct{}
+	ginCtx      *gin.Context
+	userName    string
+	conn        *websocket.Conn
+	manager     *Manager
+	question    types.Question
+	writeWaitCh chan []string
+	lenOfWait   int
 }
 
 func NewClient(userName string, conn *websocket.Conn, manager *Manager, ctx *gin.Context) *Client {
 
 	return &Client{
-		ginCtx:   ctx,
-		userName: userName,
-		conn:     conn,
-		manager:  manager,
-		exitCh:   make(chan struct{}),
+		ginCtx:      ctx,
+		userName:    userName,
+		conn:        conn,
+		manager:     manager,
+		writeWaitCh: make(chan []string),
 	}
 }
 
 func (c *Client) ReadPump() {
 	defer func() {
 		log.Println("READ PUMP : exited readpump goroutine of client: ", c.userName)
-		close(c.exitCh)
 		c.manager.DeleteClientFromConnectionPool(c)
 	}()
 	for {
@@ -64,24 +63,12 @@ func (c *Client) ReadPump() {
 		}
 		var data types.RequestData
 
-		// log.Println("data:", data)
-		// var data interface{}
 		json.Unmarshal(txt, &data)
-		log.Println("Data , headers : : ", data.Headers)
-		log.Println("Data , answer : : ", data.Answer)
-		if data.Answer == c.question.CorrectAnswer {
-			c.manager.mu.Lock()
-			c.manager.points[c.userName]++
-			c.manager.mu.Unlock()
-		}
-		//score of points
+		c.manager.HandlePointScoreness(c, data)
 		c.manager.mu.Lock()
-		log.Println("state of client ", c.userName, " before submit :", c.manager.done)
-		log.Println("client ", c.userName, ", is ready")
-		c.manager.done[c] = true
-		log.Println("state of client ", c.userName, " after submit :", c.manager.done)
-		log.Println("current score of ", c.userName, " :", c.manager.points[c.userName])
+		c.manager.doneMap[c] = true
 		c.manager.mu.Unlock()
+		log.Println("READ PUMP : client : ", c.userName, " answered question")
 	}
 
 }
@@ -89,7 +76,7 @@ func (c *Client) ReadPump() {
 func (c *Client) WritePump() {
 	defer func() {
 		log.Println("WRITE PUMP : exited writepump goroutine of client: ", c.userName)
-
+		close(c.writeWaitCh)
 	}()
 	for {
 		select {
@@ -98,7 +85,7 @@ func (c *Client) WritePump() {
 			return
 		case q, ok := <-c.manager.broadcast:
 			if !ok {
-				log.Println("channel closed while writing to user")
+				log.Println("WRITE PUMP : channel closed while writing to user")
 				return
 			}
 			if q.Id == c.question.Id {
@@ -111,24 +98,44 @@ func (c *Client) WritePump() {
 
 				err := c.conn.WriteMessage(websocket.TextMessage, buffer.Bytes())
 				if err != nil {
-					log.Println("error when writing message: ", err)
-
+					log.Println("WRITE PUMP : error when writing message: ", err)
 				}
 				continue
 			}
 
 			c.question = q
-			log.Println("currenct question : ", q)
+			log.Println("current question : ", q)
 			comp := components.Question(q)
 			buffer := &bytes.Buffer{}
 			comp.Render(context.Background(), buffer)
 
 			err := c.conn.WriteMessage(websocket.TextMessage, buffer.Bytes())
 			if err != nil {
-				log.Println("error when writing message: ", err)
+				if err == websocket.ErrCloseSent {
+					log.Println("WRITE PUMP : no connection was established ")
+					return
+				}
+				log.Println("WRITE PUMP : error when writing message: ", err)
 
 			}
+		case list, ok := <-c.writeWaitCh:
+			if !ok {
+				log.Println("WRITE PUMP : error when reading from writeWaitCh of user , : ", c.userName)
+				return
+			}
+			if len(list) == c.lenOfWait {
+				continue
+			}
+			c.lenOfWait = len(list)
 
+			comp := components.Waiting(list)
+			buffer := &bytes.Buffer{}
+			comp.Render(context.Background(), buffer)
+
+			err := c.conn.WriteMessage(websocket.TextMessage, buffer.Bytes())
+			if err != nil {
+				log.Println("WRITE PUMP : error when writing message: ", err)
+			}
 		}
 
 	}
