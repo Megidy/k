0a7a896d9f47ce2c9338ff4878bff95d18ab3eda
Megidy <byrka.sergiy@gmail.com>
2024-12-23 18:02:26 +0200

    Added new div for time showing

--- static/game/game_templ.go
+++ static/game/game_templ.go
@@ -45,7 +45,7 @@ func Game(roomID string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><div id=\"game\"><p>Waiting for other players</p></div></div></body>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><div id=\"time\"></div><div id=\"game\"><p>Waiting for other players</p></div></div></body>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
