a5e70b5fb4e9b65791cecf310f60d3ec88a44640
Megidy <byrka.sergiy@gmail.com>
2024-12-25 16:58:29 +0200

    Rewrited whole manager logic: 1)message queue : instead of using infinite broadcast channel , was made decision to use channels : 1 is when all users are ready and write the question for them ,2 is when someone connects durring the question , so it overwrites the question for him. 3 is writing "waitlist" for all client who answered the question.4 is when client who was ready leaved and connected, in this case it overwrites the list for him individually This Message queue is more efficent and more structured. 2)added some fields such as channels to structure to specify the work of each function 3)Question handler : now , instead of implementing questionHandler in message queue i made it as separate fuction which gives opportunity to change question when "waitlist" is empty , this means when everyone is ready -> change question, pretty simple and more functional architecture 4) waitListHandler: this function handlign removing or adding client to the "waitlist" 5) clientsStatusHandler: handlign the current status of user , will add score pointing later.

--- pkg/services/game/manager.go
+++ pkg/services/game/manager.go
@@ -4,38 +4,35 @@ import (
 	"context"
 	"log"
 	"sync"
-	"time"
 
 	"github.com/Megidy/k/types"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	// ReadBufferSize:  1024,
-	// WriteBufferSize: 1024,
-	// CheckOrigin: func(r *http.Request) bool {
-	// 	return false
-	// },
-}
+var upgrader = websocket.Upgrader{}
 
 //TO DO :
 
-//!!!! make password !!
+//1 : make password
 
-//overwrite check readiness not with map of ready clients but as a channel sent events :
-//1 : client answering
-//2 : submiting score
-//3 : sending event to the doneCh
-//4 : if count==len(m.clients) than currQuestion++ && count=0
-//5 : count++
+//2 : when client connects twice from one account make second request unavaible ,maybe 401 or smth like that
 
-// implement not with the sleep but with the members passsed the current question
+//3 : make normal leave and connection while game for players who was in stock -> write normal isReady statement.
+//
+//	there are 1 solution probably made , but implement this right and add timer.
+
+// 4 : when new client connects , those who answered qeustion dont see him in waitList
 type Manager struct {
+	//statement of game
+	//0 - not started yet
+	//1 - in progress
+	//-1 - ended
+	statementOfGame int
 	//unique ID of room
 	roomID string
 	//number of members
-	numberOfPlayers int
+	maxPlayers int
 	//number of questions
 	numberOfQuestions int
 	//mutex for concurenct safe reading and writing
@@ -44,222 +41,264 @@ type Manager struct {
 	ctx context.Context
 	//cancel func of context
 	cancel context.CancelFunc
-	//map of usernames
-	usernames map[string]bool
-	//points score
-	points map[string]int
-	//map of all clients
-	clients map[*Client]bool
 	//number of current question
-	currQuestion int
-	//slice of current questions
+	numberOfCurrentQuestion int
+	//questions
 	questions []types.Question
-	//chan to broadcast questions to all users
-	broadcast chan types.Question
-	//doneCh to comunicate with message queue
-	doneCh chan bool
-	//donemap to write clients who is done and who is not
-	doneMap map[*Client]bool
-	//chan to send to curr client that havent answered to the question yet
-	doneWaitCh chan []string
-	//accessCh to start game
-	accessCh chan bool
+	//curr question
+	currentQuestion types.Question
+	//clientsMap
+	clientsMap map[string]*Client
+	//stockMap for not repition of clients
+	stockMap map[string]*Client
+	//change question chan
+	changeQuestionCh chan bool
+	//change list for all clients
+	changeListCh chan bool
+	//overwrite question to new client
+	overwriteQuestionCh chan string
+	//overwrite list to new client if he answered question before
+	overwriteListCh chan string
+	//list of players who havent answered question yet
+	waitList []string
+	//channel for getting clients request about readiness
+	readyCh chan *Client
+	//channel to handle safe clients leave
+	clientsLeaveCh chan *Client
+	//channel to handle th connection
+	clientsConnectionCh chan *Client
+	//channel to start the game
+	startGameCh chan bool
+	//channel to add client to waitList after he connected to the game
+	addClientToWaitList chan *Client
+	//channel to remove client from wait list after he disconnected from the game
+	removeClientFromWaitList chan *Client
+	//channel to update notify when shoul question has to be updated
+	updateQuestionCh chan bool
 }
 
-func NewManager(id string, numberOfPlayers, amountOfQuestions int, questions []types.Question) *Manager {
+func NewManager(roomID string, numberOfPlayers, amountOfQuestions int, questions []types.Question) *Manager {
 	ctx, cancel := context.WithCancel(context.Background())
-
 	manager := &Manager{
-		roomID:            id,
-		numberOfPlayers:   numberOfPlayers,
-		numberOfQuestions: amountOfQuestions,
-		mu:                sync.Mutex{},
-		ctx:               ctx,
-		cancel:            cancel,
-		points:            make(map[string]int),
-		clients:           make(map[*Client]bool),
-		doneMap:           make(map[*Client]bool),
-		usernames:         make(map[string]bool),
-		broadcast:         make(chan types.Question),
-		doneCh:            make(chan bool, 1),
-		accessCh:          make(chan bool),
-		doneWaitCh:        make(chan []string),
-		questions:         questions,
-		currQuestion:      0,
+
+		roomID:                   roomID,
+		maxPlayers:               numberOfPlayers,
+		numberOfQuestions:        amountOfQuestions,
+		questions:                questions,
+		mu:                       sync.Mutex{},
+		ctx:                      ctx,
+		cancel:                   cancel,
+		currentQuestion:          questions[0],
+		clientsMap:               make(map[string]*Client),
+		stockMap:                 make(map[string]*Client),
+		changeQuestionCh:         make(chan bool),
+		changeListCh:             make(chan bool),
+		overwriteQuestionCh:      make(chan string),
+		overwriteListCh:          make(chan string),
+		readyCh:                  make(chan *Client),
+		startGameCh:              make(chan bool),
+		clientsConnectionCh:      make(chan *Client),
+		clientsLeaveCh:           make(chan *Client),
+		addClientToWaitList:      make(chan *Client),
+		removeClientFromWaitList: make(chan *Client),
+		updateQuestionCh:         make(chan bool),
 	}
 	go manager.MessageQueue()
-	go manager.CheckReadiness()
+	go manager.ClientsStatusHandler()
+	go manager.QuestionHandler()
+	go manager.WaitListHandler()
 	go manager.StartGame()
 
 	return manager
 }
 
-func (m *Manager) HandlePointScoreness(client *Client, requestData types.RequestData) {
-	if requestData.Answer == client.question.CorrectAnswer {
-		m.mu.Lock()
-		m.points[client.userName]++
-		m.mu.Unlock()
-	}
-
-}
-
-func (m *Manager) CheckDuplicates(client *Client) bool {
-	_, ok := m.usernames[client.userName]
-	return ok
-}
-
-func (m *Manager) AddClientToConnectionPool(client *Client) {
-
+func (m *Manager) AddClientToConnectionPool(client *Client) bool {
+	var wasInGameBefore bool
 	m.mu.Lock()
-	_, ok := m.points[client.userName]
-	if !ok {
-		m.points[client.userName] = 0
+	//checking if client was connected before
+	c, ok1 := m.stockMap[client.userName]
+	if ok1 {
+		if c.currQuestion == m.numberOfCurrentQuestion {
+			if c.isReady {
+				client.isReady = true
+			} else {
+				client.isReady = false
+			}
+		} else {
+			client.isReady = false
+		}
+
+		delete(m.stockMap, c.userName)
+		wasInGameBefore = true
+	} else {
+		wasInGameBefore = false
 	}
-	m.usernames[client.userName] = true
-	m.clients[client] = true
-	m.doneMap[client] = false
+	// c2, ok2 := m.clientsMap[client.userName]
+	// if !ok1 && ok2 {
+
+	// }
+	m.clientsMap[client.userName] = client
+	log.Println("Added new client : ", client.userName)
 	m.mu.Unlock()
-	log.Println("added new client with: ", client)
-	log.Println("currenct pool of connection: ", m.clients)
+
+	return wasInGameBefore
 }
 
 func (m *Manager) DeleteClientFromConnectionPool(client *Client) {
 	m.mu.Lock()
-	delete(m.usernames, client.userName)
-	delete(m.clients, client)
-	delete(m.doneMap, client)
-	log.Println("deleted connection from pool ", client)
-	log.Println("current connections : ", m.clients)
+	m.removeClientFromWaitList <- client
+	m.stockMap[client.userName] = client
+	delete(m.clientsMap, client.userName)
 	m.mu.Unlock()
 }
 
 // implement function start the game
 func (m *Manager) NewConnection(c *gin.Context) {
+	//getting user from data
 	u, _ := c.Get("user")
 	user := u.(*types.User)
+	//upgrading connection aka 'handshake'
 	wsConn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		log.Println("error while creating websocket connection : ", err)
 		return
 	}
-	// checking if user with this
-	// if m.CheckDuplicates(&Client{userName: user.UserName}) {
-	// 	return
-	// }
+	//creating new client
 	client := NewClient(user.UserName, wsConn, m, c)
-	m.AddClientToConnectionPool(client)
-	<-m.accessCh
-	go client.WritePump()
-	go client.ReadPump()
+
+	//adding him to connection pool, meanwhile cheking if he was connected before
+	wasInGameBefore := m.AddClientToConnectionPool(client)
+
+	//start of the game
+	<-m.startGameCh
+	//starting w/r pumps
+	m.clientsConnectionCh <- client
+
+	if wasInGameBefore {
+		if client.isReady {
+			m.overwriteListCh <- client.userName
+		} else {
+			m.addClientToWaitList <- client
+			m.overwriteQuestionCh <- client.userName
+		}
+
+	} else {
+		if m.statementOfGame == 1 {
+			log.Println("d")
+			m.addClientToWaitList <- client
+			m.overwriteQuestionCh <- client.userName
+		}
+		log.Println("started goroutine for new client : ", client.userName)
+	}
+
 }
 
 func (m *Manager) MessageQueue() {
-	defer func() {
-		log.Println("MESSAGE QUEUE: exited from goroutine")
-	}()
 	for {
-		time.After(time.Second * 10)
 		select {
-		case done, ok := <-m.doneCh:
-			if !ok {
-				log.Println("MESSAGE QUEUE: failed to read from doneCh : message queue")
-				return
+		//change question for all connected clients
+		case <-m.changeQuestionCh:
+			m.mu.Lock()
+			for _, client := range m.clientsMap {
+				m.waitList = append(m.waitList, client.userName)
+				client.currQuestion = m.numberOfCurrentQuestion
+				client.isReady = false
+				client.questionCh <- m.currentQuestion
 			}
-			if m.currQuestion < m.numberOfQuestions {
-				if done {
-					m.currQuestion++
+			m.mu.Unlock()
+		//write curr question for new client
+		case <-m.changeListCh:
+			m.mu.Lock()
+			for _, client := range m.clientsMap {
+				if client.isReady {
+					client.writeWaitCh <- m.waitList
 				}
 			}
-			if m.currQuestion == m.numberOfQuestions {
-				doneQuestion := types.Question{
-					Id:       "ID-leaderBoard",
-					Question: "leaderboard",
-				}
-				m.mu.Lock()
-				for i := 0; i < len(m.clients)*10; i++ {
-					m.broadcast <- doneQuestion
-				}
-				m.mu.Unlock()
-				m.cancel()
-				close(m.broadcast)
-				return
-			}
-		default:
-			m.broadcast <- m.questions[m.currQuestion]
+			m.mu.Unlock()
+		case username := <-m.overwriteQuestionCh:
+			m.mu.Lock()
+
+			client := m.clientsMap[username]
+			client.isReady = false
+			client.currQuestion = m.numberOfCurrentQuestion
+			client.questionCh <- m.currentQuestion
+			m.mu.Unlock()
+		//write list of not done clients if client is answered question
+		case username := <-m.overwriteListCh:
+			m.mu.Lock()
+			client := m.clientsMap[username]
+			client.writeWaitCh <- m.waitList
+			m.mu.Unlock()
 		}
-
 	}
-
 }
 
-func (m *Manager) CheckReadiness() {
+func (m *Manager) ClientsStatusHandler() {
 
 	for {
 		select {
-		case <-m.ctx.Done():
-			log.Println("CHECK READINESS : exited goroutine via ctx")
-			close(m.doneCh)
-			return
-		default:
-			var count int
-			var check int
-			m.mu.Lock()
-			count = len(m.clients)
-			m.mu.Unlock()
-			if count == 0 {
-				continue
-			}
-			m.mu.Lock()
-			var list []string
-			for client, ready := range m.doneMap {
-				if ready {
-					check++
+		case client := <-m.readyCh:
+			client.isReady = true
+			m.removeClientFromWaitList <- client
 
-				} else {
-					list = append(list, client.userName)
-				}
-
-				if count == check {
-					for k := range m.doneMap {
-						m.doneMap[k] = false
-					}
-					m.doneCh <- true
-				}
-			}
-			m.mu.Unlock()
-			m.doneWaitCh <- list
-			check = 0
-
+		case <-m.clientsLeaveCh:
+		case client := <-m.clientsConnectionCh:
+			go client.ReadPump()
+			go client.WritePump()
 		}
 
 	}
 }
 
-func (m *Manager) WriteNotDoneUsers() {
+func (m *Manager) WaitListHandler() {
 	for {
 		select {
-		case <-m.ctx.Done():
-
-		case list := <-m.doneWaitCh:
-			m.mu.Lock()
-			for client, ready := range m.doneMap {
-				if ready {
-					client.writeWaitCh <- list
+		case client := <-m.addClientToWaitList:
+			m.waitList = append(m.waitList, client.userName)
+			m.changeListCh <- true
+		case client := <-m.removeClientFromWaitList:
+			for index, value := range m.waitList {
+				if value == client.userName {
+					m.waitList = append(m.waitList[:index], m.waitList[index+1:]...)
 				}
 			}
-			m.mu.Unlock()
+			if len(m.waitList) == 0 {
+				m.updateQuestionCh <- true
+			} else {
+				m.changeListCh <- true
+			}
 		}
-
 	}
+}
 
+func (m *Manager) QuestionHandler() {
+	for {
+		select {
+		case <-m.updateQuestionCh:
+			if m.numberOfCurrentQuestion == len(m.questions)-1 {
+
+				//later : handler end of the game
+				m.numberOfCurrentQuestion = 0
+			} else {
+				m.numberOfCurrentQuestion++
+
+			}
+			//changing question and sending message to channel that everyone is ready and new question can be delivered
+			m.currentQuestion = m.questions[m.numberOfCurrentQuestion]
+			m.changeQuestionCh <- true
+		}
+	}
 }
 
 func (m *Manager) StartGame() {
 	for {
-		if len(m.clients) == m.numberOfPlayers {
-			go m.WriteNotDoneUsers()
-			m.accessCh <- true
-			close(m.accessCh)
+		// log.Println("clients pool : ", len(m.clientsMap))
+		if len(m.clientsMap) == m.maxPlayers {
+			log.Println("GAME STARTED !!")
+			m.startGameCh <- true
+			m.changeQuestionCh <- true
+			close(m.startGameCh)
+			m.statementOfGame = 1
 			return
 		}
 	}
