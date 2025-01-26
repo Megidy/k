35abe0c6118b40b37de02e074f855d783309d8ee
Megidy <byrka.sergiy@gmail.com>
2025-01-26 20:26:25 +0200

    Added profile picture handling and user description update functionality

--- pkg/services/user/store.go
+++ pkg/services/user/store.go
@@ -2,9 +2,18 @@ package user
 
 import (
 	"database/sql"
+	"math/rand"
 
 	"github.com/Megidy/k/types"
 )
+
+var pictures = []string{
+	"https://i.pinimg.com/736x/b0/93/c5/b093c578f3c99b2525194db73cf12e01.jpg",
+	"https://i.pinimg.com/736x/b2/7f/31/b27f31e8ddc96d54536e1f162948272a.jpg",
+	"https://i.pinimg.com/736x/f8/b2/20/f8b220ee6d7f3c12b9c1ba3f202a5813.jpg",
+	"https://i.pinimg.com/736x/6e/89/fa/6e89faaffc9ff42df7167c60abf6775c.jpg",
+	"https://i.pinimg.com/736x/f0/07/f5/f007f57c6092bf3ca8189756de467365.jpg",
+}
 
 type store struct {
 	db *sql.DB
@@ -21,7 +30,7 @@ func (s *store) GetUserById(id string) (*types.User, error) {
 	}
 	var user types.User
 	for row.Next() {
-		err = row.Scan(&user.ID, &user.UserName, &user.Email, &user.Password)
+		err = row.Scan(&user.ID, &user.UserName, &user.Email, &user.Password, &user.Description, &user.ProfilePicture)
 		if err != nil {
 			return nil, err
 		}
@@ -29,7 +38,9 @@ func (s *store) GetUserById(id string) (*types.User, error) {
 	return &user, nil
 }
 func (s *store) CreateUser(user *types.User) error {
-	_, err := s.db.Exec("insert into users values(?,?,?,?)", user.ID, user.UserName, user.Email, user.Password)
+	user.ProfilePicture = pictures[rand.Intn(5)]
+
+	_, err := s.db.Exec("insert into users(id,username,email,password,profile_picture) values(?,?,?,?,?)", user.ID, user.UserName, user.Email, user.Password, user.ProfilePicture)
 	if err != nil {
 		return err
 	}
@@ -53,10 +64,20 @@ func (s *store) GetUserByEmail(email string) (*types.User, error) {
 	}
 	var user types.User
 	for rows.Next() {
-		err = rows.Scan(&user.ID, &user.UserName, &user.Email, &user.Password)
+		err = rows.Scan(&user.ID, &user.UserName, &user.Email, &user.Password, &user.Description, &user.ProfilePicture)
 		if err != nil {
 			return nil, err
 		}
 	}
 	return &user, nil
+}
+
+func (s *store) UpdateDescription(userID, description string) error {
+	_, err := s.db.Exec("update users set description=? where id=?", description, userID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
