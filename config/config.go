8282b6e0c4834aff8272fe599bfe62285762edec
Megidy <byrka.sergiy@gmail.com>
2025-02-11 21:17:30 +0200

    Added default values for configuration parameters in config.go

--- config/config.go
+++ config/config.go
@@ -7,28 +7,28 @@ import (
 )
 
 type Config struct {
-	//HTTP configuration
-	HTTPPort string `env:"HTTP_PORT"`
+	// HTTP configuration
+	HTTPPort string `env:"HTTP_PORT" envDefault:":8080"`
 
-	//Worker configuration
-	NumberOfWorkers int `env:"NUMBER_OF_WORKERS"`
+	// Worker configuration
+	NumberOfWorkers int `env:"NUMBER_OF_WORKERS" envDefault:"100"`
 
-	//JWT configuration
-	JWTSecretKey string `env:"JWT_SECRET_KEY"`
+	// JWT configuration
+	JWTSecretKey string `env:"JWT_SECRET_KEY" envDefault:"f0834ujf90r3fhrju38fc9r3hf093hr3"`
 
-	//Manager configuration
-	TimeForAnswer          int `env:"TIME_FOR_ANSWER"`
-	TimeForRoomLiquidation int `env:"TIME_FOR_ROOM_LIQUDATION"`
+	// Manager configuration
+	TimeForAnswer          int `env:"TIME_FOR_ANSWER" envDefault:"20"`
+	TimeForRoomLiquidation int `env:"TIME_FOR_ROOM_LIQUDATION" envDefault:"360"`
 
-	//MySQL configuration
-	MySQLConnectionString string `env:"MYSQL_CONNECTION_STRING"`
-	MySQLRootPassword     string `env:"MYSQL_ROOT_PASSWORD"`
-	MySQLDatabase         string `env:"MYSQL_DATABASE"`
+	// MySQL configuration
+	MySQLConnectionString string `env:"MYSQL_CONNECTION_STRING" envDefault:"root:password@tcp(mysql:3306)/k"`
+	MySQLRootPassword     string `env:"MYSQL_ROOT_PASSWORD" envDefault:"password"`
+	MySQLDatabase         string `env:"MYSQL_DATABASE" envDefault:"k"`
 
-	//Redis configuration
-	RedisConnectionString         string `env:"REDIS_CONNECTION_STRING"`
-	RedisTopicCacheDuration       int    `env:"REDIS_TOPIC_CACHE_DURATION"`
-	RedisLeaderboardCacheDuration int    `env:"REDIS_LEADERBOARD_CACHE_DURATION"`
+	// Redis configuration
+	RedisConnectionString         string `env:"REDIS_CONNECTION_STRING" envDefault:"redis:6379"`
+	RedisTopicCacheDuration       int    `env:"REDIS_TOPIC_CACHE_DURATION" envDefault:"48"`
+	RedisLeaderboardCacheDuration int    `env:"REDIS_LEADERBOARD_CACHE_DURATION" envDefault:"48"`
 }
 
 func NewConfig() *Config {
